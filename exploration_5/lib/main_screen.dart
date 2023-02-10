61f917595ff27980f497510e44e09ee5a61c9c07
Matthew Pacey <paceym@oregonstate.edu>
2023-02-10 14:07:13 -0800

    Layout exploration

--- exploration_5/lib/main_screen.dart
+++ exploration_5/lib/main_screen.dart
@@ -9,7 +9,31 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text(title),),
-      body: CenteredPlaceholder(padding: 50),
+      //body: CenteredPlaceholder(padding: 50),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [placeholderRow(), placeholderRow()],
+
+      )
+
     );
+  }
+
+  Widget paddedPlaceholder(){
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: SizedBox(
+        width: 100,
+        height: 100,
+        child: Placeholder(),
+      ),
+    );
+  }
+
+  Widget placeholderRow(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [paddedPlaceholder(), paddedPlaceholder()
+      ],);
   }
 }
