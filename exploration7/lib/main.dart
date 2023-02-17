e234f3cb05022207b25a151d590e2102e4b57669
Matthew Pacey <paceym@oregonstate.edu>
2023-02-16 23:10:25 -0800

    Pausing at LayoutBuilder

--- exploration7/lib/main.dart
+++ exploration7/lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp,
+  ]);
+  //runApp(const MyApp());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Adaptive Layouts',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Adaptive Layouts'),),
+        body: Container(
+          //constraints: BoxConstraints(maxHeight: 300, maxWidth: 400, minWidth: 400),
+          child: LayoutBuilder(builder: (context, constraints){
+            return constraints.maxWidth < 500 ? VerticalLayout() : HorizontalLayout();
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class VerticalLayout extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.lightGreen);
+  }
+}
+
+class HorizontalLayout extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(child: Container(color: Colors.lightGreen)),
+        Expanded(child: Container(color: Colors.orange)),
+      ],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +58,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Explorations 7',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +71,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Adaptive Layouts')),
+        body: Center(child: Text('Builder Demo'),),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: (){
+        //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('SNAACK')));
+        //   },
+        // ),
+        // floatingActionButton: SnackFab(),
+        floatingActionButton: Builder(builder: (context){
+          return FloatingActionButton(onPressed: (){
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('SNAACK2')));
+          });
+        },),
+      )
     );
   }
 }
@@ -63,36 +124,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -106,10 +143,22 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: (){
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('SNAACK')));
+        }
+        // tooltip: 'Increment',
+        // child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class SnackFab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return FloatingActionButton(onPressed: (){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('SNAACK')));
+    });
+  }
+
 }
