4470fd9109c6a3a0e90b896ddc64d2064f1cee75
Matthew Pacey <paceym@oregonstate.edu>
2023-02-26 12:30:00 -0800

    Added frameworks and placeholders

---
+++ proj4_journal/lib/journal_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JournalApp extends StatefulWidget {
+  @override
+  State<JournalApp> createState() => _JournalAppState();
+}
+
+class _JournalAppState extends State<JournalApp> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
