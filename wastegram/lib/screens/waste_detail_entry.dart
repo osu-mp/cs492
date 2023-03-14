8340306b3d3ed31dab0fcf4b79af5fce3efe3dc5
Matthew Pacey <paceym@oregonstate.edu>
2023-03-13 19:13:55 -0700

    Input image from picker, starting input field

--- wastegram/lib/screens/waste_detail_entry.dart
+++ wastegram/lib/screens/waste_detail_entry.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
 
 class WasteDetailEntry extends StatefulWidget{
   static  final String routeName = 'WasteDetailEntry';
@@ -10,11 +13,43 @@ class WasteDetailEntry extends StatefulWidget{
 }
 
 class _WasteDetailEntryState extends State<WasteDetailEntry> {
+  File? image;
+  final ImagePicker _picker = ImagePicker();
+  String? text;
+
+  void getImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    // final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    image = File(pickedFile!.path);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget body = Text('matt');
+    if (image == null) {
+      getImage();
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('New Post'), centerTitle: true,),
-      body: Text('bas'),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(child: Image.file(image!), height:  100,),
+          SizedBox(height: 40,),
+          ElevatedButton(onPressed: (){}, child: Text('Post It')),
+          // TextField(
+          //   decoration: new InputDecoration(labelText: "Enter your number"),
+          //   keyboardType: TextInputType.number,
+          //   inputFormatters: <TextInputFormatter>[
+          //     FilteringTextInputFormatter.digitsOnly
+          //   ], // Only numbers can be entered
+          // ),
+        ],
+      )
+      ),
     );
   }
 }
